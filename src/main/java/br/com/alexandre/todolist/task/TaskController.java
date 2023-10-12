08a3e47104cd488f07236a1ed7da640ea7af23de
Alexandre Junior <alexandrejunior408@gmail.com>
2023-10-12 14:30:33 -0300

    feat: create method to list all tasks from user

--- src/main/java/br/com/alexandre/todolist/task/TaskController.java
+++ src/main/java/br/com/alexandre/todolist/task/TaskController.java
@@ -1,11 +1,13 @@
 package br.com.alexandre.todolist.task;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,4 +41,10 @@ public class TaskController {
         return ResponseEntity.status(HttpStatus.OK).body(task);
     }
 
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request) {
+        var userId = (UUID) request.getAttribute("userId");
+        var tasks = this.taskRepository.findByUserId(userId);
+        return tasks;
+    }
 }
