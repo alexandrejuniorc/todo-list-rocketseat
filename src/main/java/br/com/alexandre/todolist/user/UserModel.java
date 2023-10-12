80847979a7350cb9120d8ee53e824048f10c3883
Alexandre Junior <alexandrejunior408@gmail.com>
2023-10-11 21:55:59 -0300

    feat: config properties in UserModel

--- src/main/java/br/com/alexandre/todolist/user/UserModel.java
+++ src/main/java/br/com/alexandre/todolist/user/UserModel.java
@@ -1,7 +1,31 @@
 package br.com.alexandre.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data // set all getters and setters for variables
+// @Getter set getters for all variables
+// @Setter set setters for all variables
+@Entity(name = "tb_users")
 public class UserModel {
-    public String userName;
-    public String name;
-    public String password;
+    @Id
+    @GeneratedValue(generator = "UUID") // generate automatic id
+    private UUID id;
+
+    // @Column(name = "usuario") set column name
+    @Column(unique = true)
+    private String username;
+    private String name;
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
